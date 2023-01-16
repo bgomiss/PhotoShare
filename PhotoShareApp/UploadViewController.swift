c373e5982bbad7b44c5204ee16f30a2cd8492968
bgomiss <aduskun@gmail.com>
2023-01-16 16:51:17 +0300

    upload image

--- PhotoShareApp/UploadViewController.swift
+++ PhotoShareApp/UploadViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
-
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageSelection))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
+    @objc func imageSelection() {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func uploadTapped(_ sender: UIButton) {
+        
+        
+    }
+    
 
 }
